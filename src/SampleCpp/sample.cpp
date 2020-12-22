61d79de1bb56bf3df6e0b127630890689b8135d0
Oğuz Karan <oguzkaran@csystem.org>
2020-12-22 14:39:55 +0300

    Arcelik-Eskisehir-C++

--- src/SampleCpp/sample.cpp
+++ src/SampleCpp/sample.cpp
@@ -1,24 +1,18 @@
 ﻿/*----------------------------------------------------------------------------------------------------------------------
-	Aday Fonksiyonlar: 1, 2, 3, 4, 5
-	Uygun fonksiyonlar: 2, 3
+	Aşağıdaki örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-void foo();							//1
-void foo(int);						//2
-void foo(double, double = 3.4);		//3
-void foo(char*);					//4
-void foo(int, int);					//5
 
 void bar()
 {
-	foo(4);
+	
 }
 
 int main()
-{	
+{
+	std::nullptr_t a;
 
-	
+	a = 0;	
 	return 0;
 }
