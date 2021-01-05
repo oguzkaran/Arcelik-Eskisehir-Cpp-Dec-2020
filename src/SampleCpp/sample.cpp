a5d5fc1021f896dab6452c235c9c99af98572354
Oğuz Karan <oguzkaran@csystem.org>
2021-01-05 14:33:17 +0300

    Arcelik-C++

--- src/SampleCpp/sample.cpp
+++ src/SampleCpp/sample.cpp
@@ -1,32 +1,33 @@
 ﻿/*----------------------------------------------------------------------------------------------------------------------
-	C++'da sınıfların üye fonksiyonları (member functions) olabilir. Üye fonksiyonlar sınıf bildirimi içerisinde veya
-	sınıf bildirimi dışında tanımlanabilir. Sınıf bildirimi içerisinde tanımlanan fonksiyonlar inline tanımlanmış
-	kabul edilir. Üye fonksiyonun sınıf dışında tanımlanması sınıf ismi :: ile yapılır
+	Sınıfın non-static elemanlarına sınıf ismi ve :: operatörü ile erişilemez
 ----------------------------------------------------------------------------------------------------------------------*/
 #include <iostream>
 
 class Sample {
 public:
-	int a;
-	void foo()
-	{
-		//...
-	}
+	int a;	
+	static bool b;
+	//...
+	static void foo();
 	void bar();
-private:
-	double b;
 };
+
+bool Sample::b;
+
+void Sample::foo()
+{
+	std::cout << "Sample::foo\n";
+}
 
 void Sample::bar()
 {
-	//...
+	std::cout << "Sample::bar\n";
 }
-
 
 int main()
-{	
-	
-	
+{
+	std::cout << Sample::a << '\n'; //error
+	Sample::bar(); //error
+
 	return 0;
 }
-
