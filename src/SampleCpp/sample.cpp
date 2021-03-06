c129bebe78c69db7bdb4a62ca13aa82e55f66836
Oğuz Karan <oguzkaran@csystem.org>
2021-03-06 23:35:19 +0300

    Arcelik-C++

--- src/SampleCpp/sample.cpp
+++ src/SampleCpp/sample.cpp
@@ -1,33 +1,30 @@
 ﻿/*----------------------------------------------------------------------------------------------------------------------
-	Yukarıdaki fill fonksiyonunun STL karşılığı olan generate_n fonksiyonunun kullanımı
+	priority_queue sınıfı
 ----------------------------------------------------------------------------------------------------------------------*/
 #include <iostream>
-#include <list>
-#include <vector>
+#include <numeric>
+#include <queue>
 #include <deque>
-#include <cstdlib>
-#include <ctime>
-#include <iterator>
-#include <algorithm>
+#include <list>
+#include <functional>
 #include "Temperature.hpp"
 
 int main()
 {
 	using namespace std;
-	using com::arcelik::Temperature;
+	using Temp = com::arcelik::Temperature;
 
-	srand(static_cast<unsigned int>(time(nullptr)));
+	vector<Temp> tv(11);
 
-	vector<Temperature> tvec;	
-	list<Temperature> tlist;
-	
-	generate_n(back_inserter(tvec), 10, [min = -65, max = 95]{ return Temperature{rand() % (max - min) + min}; });
-	for_each(cbegin(tvec), cend(tvec), [](const auto& t) {cout << t << ' ';});
+	iota(begin(tv), end(tv), Temp{ -5 });
+	priority_queue<Temp> pq(less<Temp>(), tv);
+
+	while (!pq.empty()) {
+		cout << pq.top() << ' ';
+		pq.pop();
+	}
+
 	cout << '\n';
 
-	copy_if(cbegin(tvec), cend(tvec), front_inserter(tlist), [](const auto& t) {return t < 0;});
-	for_each(cbegin(tlist), cend(tlist), [](const auto& t) {cout << t << ' ';});
-	cout << '\n';
-	
 	return 0;
 }
